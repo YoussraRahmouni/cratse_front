596be3368e70352b9a3bc5b242e66f6ebede6e2c
rahmouni.youssra <youssrarahmouni41@gmail.com>
2023-02-07 16:28:47 +0000

    admin

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -10,7 +10,7 @@ import { BaseService } from './base.service';
 export class UserService {
 
   //apiUrl = 'localhost:8080/';
-  usersUrl = '../../assets/mock-data/users.json';
+  //usersUrl = '../../assets/mock-data/users.json';
   API_URL = this.baseService.API_URL;
   HTTP_OPTIONS = this.baseService.HTTP_OPTIONS;
   allUsers : User[] = [] ;
@@ -24,8 +24,28 @@ export class UserService {
   }
   getAllUsers() {
     //this.http.get<Project[]>(this.apiUrl+'projects');
-    return this.http.get<User[]>(this.usersUrl);
+    return this.http.get<User[]>(this.API_URL+'users', this.HTTP_OPTIONS);
   }
+  getAllManagers() {
+    //this.http.get<Project[]>(this.apiUrl+'projects');
+    return this.http.get<User[]>(this.API_URL+'managers', this.HTTP_OPTIONS);
+  }
+
+  // Endpoint: users/{userId}/manager
+  updateManager(idUser:any,manager:User){
+    const body = {
+      'manager': manager.idUser.toString(),
+    }
+    return this.http.put<User>(this.API_URL+'users/'+idUser+'/manager',body,this.HTTP_OPTIONS);
+  }
+
+  updateRole(idUser:any,idRole:any){
+    const body = {
+      'role': idRole.toString(),
+    }
+    return this.http.put<User>(this.API_URL+'users/'+idUser+'/role',body,this.HTTP_OPTIONS);
+  }
+
   addUser(firstName: any, lastName: any,
     email: any, password: any,
     job: any, role: any) {
